5e070c2cccaa35bf7c30ca12cc8fb9a266100870
ElleStark <elle.stark5645@gmail.com>
2025-09-13 07:15:52 -0600

    add script for read/write PIV data

---
+++ PIV_write_u_array.py
@@ -0,0 +1,75 @@
+# read in .vc7 data and write as u, v data in stacked .npy arrays
+# Elle Stark, Sept 2025
+
+import matplotlib.pyplot as plt
+import pivpy as pp
+from pivpy import io
+import numpy as np
+import logging
+from glob import glob
+import os
+
+# Set up logging for convenient messages
+logger = logging.getLogger('PIVread')
+logger.setLevel(logging.DEBUG)
+handler = logging.StreamHandler()
+handler.setFormatter(logging.Formatter("%(asctime)s:%(name)s:%(levelname)s:%(message)s"))
+logger.addHandler(handler)
+INFO = logger.info
+WARN = logger.warning
+DEBUG = logger.debug
+
+# specify source directory and get list of files. assumes PIV data is in .vc7 format
+piv_dir = 'D:/Elle/Source_config_PIV/9.10_30cms_FractalTG_diffusiveSource_53pctneuHe0.857_47pctAir0.771_PIV0.2/StereoPIV_MPd(3x12x12_75%ov)/PostProc_interp_3x3smooth/Resize'
+vc7_files = sorted(glob(os.path.join(piv_dir, '*.vc7')))
+n_frames = len(vc7_files)  # specify subset if needed
+
+QC_plot = True
+
+# specify file names for saving arrays
+ufile = 'ignore/data/u_30cmsDiffuseFractalNeutral.npy'
+vfile = 'ignore/data/v_30cmsDiffuseFractalNeutral.npy'
+xfile = 'ignore/data/x_30cmsDiffuseFractalNeutral.npy'
+yfile = 'ignore/data/y_30cmsDiffuseFractalNeutral.npy'
+
+# extract vector data for first frame to get x and y grids
+vec_data = io.load_vc7(vc7_files[0])
+x_vec, y_vec = vec_data['x'].values, vec_data['y'].values
+DEBUG(f'x_vec shape: {x_vec.shape}')
+xg, yg = np.meshgrid(x_vec, y_vec)
+DEBUG(f'x and y grid shape: {xg.shape}')
+spatial_res = x_vec[2] - x_vec[1]
+DEBUG(f'spatial resolution: {spatial_res}')
+
+# QC plot u and v if needed
+if QC_plot:
+    u_grid, v_grid = np.squeeze(vec_data['u'].values), np.squeeze(vec_data['v'].values)
+    v_grid = -1*v_grid  # vertical positive and negative directions are opposite for DaVis vs Python
+    DEBUG(f'u_vec shape: {u_grid.shape}')
+
+    vec_stride = 16
+    fig, ax = plt.subplots()
+    qv = ax.quiver(xg[::vec_stride, ::vec_stride], yg[::vec_stride, ::vec_stride], u_grid[::vec_stride, ::vec_stride], 
+                v_grid[::vec_stride, ::vec_stride], scale=15, color='black')
+    ax.set_aspect('equal', adjustable='box')
+    plt.show()
+
+# initialize .npy arrays for storing u, v data
+u_stack = np.zeros((u_grid.shape[0], u_grid.shape[1], n_frames))
+v_stack = np.zeros((v_grid.shape[0], v_grid.shape[1], n_frames))
+
+for i in range(n_frames):
+    vec_data = io.load_vc7(vc7_files[0])
+    u = np.squeeze(vec_data['u'].values)
+    v = np.squeeze(vec_data['v'].values)
+    v = -1 * v  # correct for vertical positive/negative in DaVis vs Python
+
+    u_stack[:, :, i] = u
+    v_stack[:, :, i] = v
+
+# save u, v, x, and y arrays
+np.save(ufile, u_stack)
+np.save(vfile, v_stack)
+np.save(xfile, xg)
+np.save(yfile, yg)
+
--- PLIF_main.py
+++ PLIF_main.py
@@ -2,6 +2,9 @@
 # Elle Stark Sept 2025
 
 import matplotlib.pyplot as plt
+import matplotlib.colors as colors
+import matplotlib.animation as animation
+from matplotlib import rcParams
 import numpy as np
 import cmasher as cmr
 import logging
@@ -9,20 +12,106 @@ import lvpyio as lv
 
 
 # Set up logging for convenient messages
-logger = logging.getLogger('MOXLIFTests')
+logger = logging.getLogger('PLIFplots')
 logger.setLevel(logging.DEBUG)
 handler = logging.StreamHandler()
 handler.setFormatter(logging.Formatter("%(asctime)s:%(name)s:%(levelname)s:%(message)s"))
 logger.addHandler(handler)
 INFO = logger.info
-WARN = logger.warn
+WARN = logger.warning
 DEBUG = logger.debug
 
 # read in .im7 files using lavision io package
 # set path1 to data of laser that fires first, path2 to second
-path1 = 'D:/Elle/PLIF_MOX_2023-07-21/data_r57_L3/r57_Subtract_bg_L3/AboveBelow/Divide_ff63_L3/Divide_instantaneous_sourceAvg/Reflectance_adjustment/AddCameraAttributes/ImageCorrection_calibrate.set'
-path2 = 'D:/Elle/PLIF_MOX_2023-07-21/data_r57_L3/r57_subtract_bg_L4/Above0/Divide_ff63_L4/Divide_instantaneous_sourceAvg/Reflectance_adjustment/AddCameraAttributes/ImageCorrection_calibrate.set'
-save_file = 'C:/Users/LaVision/Documents/Data_processing/r57_finalData_25to300s.npy'
+path1 = 'I:/9.10.2025_sourceConfig_PLIF/data_9.10.2025_30cms_FractalTG_diffusiveSource_53pctneuHe0.857_47pctAir0.771_PIV0.2_L4/Subtract_bg_L4/Divide_ffmbg_L4/Divide_C0_18.62/AddCameraAttributes/ImageCorrection/Resize.set'
+path2 = 'I:/9.10.2025_sourceConfig_PLIF/data_9.10.2025_30cms_FractalTG_diffusiveSource_53pctneuHe0.857_47pctAir0.771_PIV0.2_L4/Subtract_bg_L3/Divide_ffmbg_L3/Divide_C0_20.26/AddCameraAttributes/ImageCorrection/Resize.set'
+save_file_data = 'ignore/data/30cms_diffuse_fractal_neutralHe_PLIF_noSmooth.npy'
+save_file_ani = 'ignore/animations/30cms_diffuse_fractal_neutralHe_PLIFv2.mp4'
 
+# Plotting parameter selections
+vmin=0.015
+vmax=0.75
+cmap = cmr.cosmic
+norm = colors.LogNorm(vmin=vmin, vmax=vmax)
+# norm = colors.Normalize(vmin=vmin, vmax=vmax)
+QC = False  # True for individual plots at each timestep
+save_data = True
 
+# misc. user-set params
+offset = 0  # frame to start on
 
+# set up buffer objects (see lavision pyio manual)
+s1 = lv.read_set(path1)
+s2 = lv.read_set(path2)
+# check size and n frames
+buffer = s1[0]
+arr = buffer.as_masked_array()
+dims = np.shape(arr.data)
+DEBUG(dims) 
+total_frames = len(s1) + len(s2)  
+INFO(f'total frames: {total_frames}')
+#  nframes_to_plot = total_frames  # manually select subset in time if needed
+nframes_to_plot = 300
+
+# initialize array for storing collated data
+combined_data = np.zeros((dims[0], dims[1], nframes_to_plot))
+avg_sensor_ts = np.zeros(nframes_to_plot)
+avg_compare_ts = np.zeros(nframes_to_plot)
+
+# collate images into single sequential stack
+frame_list = [x+offset for x in list(range(nframes_to_plot))]
+# frame_list = range(nframes_to_plot)
+for i in frame_list:
+    # if even, read from first set, if odd from second set
+    if i%2==0:  
+        buffer = s1[int(i/2)]
+    else:
+        buffer = s2[int((i-1)/2)]
+
+    arr = buffer.as_masked_array()  # necessary conversion (see lavision pyio manual)
+    tempdata = arr.data  # check if needed
+
+    # QC plot: data image with sensor box overlay
+    if QC:
+        plt.imshow(tempdata, cmap=cmap, norm=norm)
+        plt.colorbar()
+        plt.show()
+
+    combined_data[:, :, i-offset] = tempdata
+
+DEBUG(f'combined data dims: {combined_data.shape}')
+
+# save stack of raw data (if desired)
+if save_data:
+    np.save(save_file_data, combined_data)
+
+##### CREATE ANIMATION ######
+
+# Set up figure and axes
+fig, ax = plt.subplots(figsize=(25.6, 14.4))
+
+# Initialize image plot
+combined_data[combined_data<vmin] = vmin
+img_display = ax.imshow(combined_data[:, :, 0], cmap=cmap, norm=norm)
+plt.colorbar(img_display)
+ax.set_aspect('equal')
+ax.set_title("30 cm/s Diffusive source, fractal grid, neutral He")
+ax.set_xticks([])
+ax.set_yticks([])
+
+# Animation function
+def update(frame):
+    img_display.set_array(combined_data[:, :, frame])  # Update image
+    return img_display
+
+# Create animation
+ani = animation.FuncAnimation(fig, update, frames=nframes_to_plot, interval=50, blit=False)
+# Set FFmpeg writer path
+rcParams['animation.ffmpeg_path'] = r"C:/Users/LaVision/AppData/Local/Programs/ffmpeg-7.1-essentials_build/bin/ffmpeg.exe"  # Replace with your actual path
+
+# Create writer object
+Writer = animation.FFMpegWriter(fps=20, bitrate=1800)
+
+# Save or display animation
+ani.save(save_file_ani, writer=Writer, dpi=150)  
+plt.show()
