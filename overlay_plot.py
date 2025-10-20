c7b647d1f0f06bcffc9b4263fae7b3a45e2dcb0f
ElleStark <elle.stark5645@gmail.com>
2025-10-20 06:31:35 -0600

    minor

--- overlay_plot.py
+++ overlay_plot.py
@@ -7,10 +7,12 @@ import matplotlib.colors as colors
 from matplotlib import rcParams
 import cmasher as cmr
 
+frame_to_plot = 10
+
 # load data
-plif_data = np.load()
-v_data = np.load('ignore/data/v_30cmsDiffuseFractalNeutral.npy')
-u_data = np.load('ignore/data/u_30cmsDiffuseFractalNeutral.npy')
+plif_data = np.load('D:/GFM/GFM/ignore/data/30cms_diffuse_fractal_neutralHe_PLIF.npy')[:, :, frame_to_plot]
+v_data = np.load('ignore/data/v_30cmsDiffuseFractalNeutral.npy')[:, :, frame_to_plot]
+u_data = np.load('ignore/data/u_30cmsDiffuseFractalNeutral.npy')[:, :, frame_to_plot]
 x = np.load('ignore/data/x_30cmsDiffuseFractalNeutral.npy')
 y = np.load('ignore/data/y_30cmsDiffuseFractalNeutral.npy')
 x_min = np.min(x)
@@ -25,7 +27,7 @@ vmin= 0.0
 vmax= 0.5
 cmap = cmr.chroma
 norm = colors.Normalize(vmin=vmin, vmax=vmax)
-frame_to_plot = 40
+
 
 vel_mag = np.sqrt(u_data**2 + v_data**2)
 
@@ -37,20 +39,25 @@ fig, ax = plt.subplots(figsize=(25.6, 14.4))
 
 # quiver plot
 vec_stride = 18
-qv = ax.quiver(x[::vec_stride, ::vec_stride], y[::vec_stride, ::vec_stride], u_data[::vec_stride, ::vec_stride, 0], 
-            v_data[::vec_stride, ::vec_stride, 0], vel_mag[::vec_stride, ::vec_stride, 0], norm=norm, scale=20, headlength=3.65, headaxislength=3, cmap=cmap)
+qv = ax.quiver(x[::vec_stride, ::vec_stride], y[::vec_stride, ::vec_stride], u_data[::vec_stride, ::vec_stride], 
+            v_data[::vec_stride, ::vec_stride], vel_mag[::vec_stride, ::vec_stride], norm=norm, scale=20, headlength=3.65, headaxislength=3, cmap=cmap)
 cbar = fig.colorbar(qv, ax=ax, label='Magnitude')
 ax.set_aspect('equal', adjustable='box')
 ax.set_xticks([])
 ax.set_yticks([])
 
 # overlay PLIF plot
+# Plotting parameter selections
+vmin=0.015
+vmax=0.75
+cmap = cmr.cosmic
+norm = colors.LogNorm(vmin=vmin, vmax=vmax)
 plif_data[plif_data<vmin] = vmin
-img_display = ax.imshow(plif_data[:, :, 0], cmap=cmap, norm=norm)
+img_display = ax.imshow(plif_data[:, :], cmap=cmap, norm=norm, alpha=1, extent=[x_min, x_max, y_min, y_max])
 plt.colorbar(img_display)
 # ax.set_aspect('equal')
 ax.set_xticks([])
 ax.set_yticks([])
 
-
+plt.savefig(save_file, dpi=300)
 plt.show()
